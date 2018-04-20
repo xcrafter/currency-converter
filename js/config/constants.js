03e135d55bce3f486b5e8836aa8d9eb1108ba627
Manzoor Samad <manzoorsamad.in@gmail.com>
2018-04-20 20:43:59 +0400

    added constants

---
+++ js/config/constants.js
@@ -0,0 +1,13 @@
+export const AppColorTheme = {
+  color_primay: '#27ae60',
+  color_secondary: '#2ecc71',
+  active_color: '#27ae60',
+  inactive_color: '#34465d',
+  text_primary: '#131418',
+  dark_text: '#1e2126',
+  light_text: '#fff',
+};
+export default {
+  base_url: 'https://txf-ecb.glitch.me/rates',
+};
+
